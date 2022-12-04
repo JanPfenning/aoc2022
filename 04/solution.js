619572c484427055f2272a0530f4bdccbd46b4e1
Jan Pfenning <jan.pfenning@gmail.com>
2022-12-04 12:48:49 +0100

    make readable

--- 04/solution.js
+++ 04/solution.js
@@ -4,22 +4,18 @@ let fileContent = fs.readFileSync(fileName, 'utf8');
 
 const includingPairs = fileContent.split(/\r?\n/)
     .map(pairLine => pairLine.match(/\d*/g).filter(group => group).map(numberString => Number(numberString)))
-    //.forEach(pair => console.log(pair))
     .filter(boundries => boundries[2]>=boundries[0] && boundries[3]<=boundries[1]
                       || boundries[0]>=boundries[2] && boundries[1]<=boundries[3])
-    //.forEach(filtered => console.log(filtered))
     .length
 console.log(includingPairs)
 
 const overlappingPairs = fileContent.split(/\r?\n/)
     .map(pairLine => pairLine.match(/\d*/g).filter(group => group).map(numberString => Number(numberString)))
-    //.forEach(pair => console.log(pair))
     .filter(boundries =>
            boundries[2] <= boundries[0] && boundries[0] <= boundries[3]
         || boundries[2] <= boundries[1] && boundries[1] <= boundries[3]
         || boundries[0] <= boundries[2] && boundries[2] <= boundries[1]
         || boundries[0] <= boundries[3] && boundries[3] <= boundries[1]
     )
-    //.forEach(filtered => console.log(filtered))
     .length
 console.log(overlappingPairs)
