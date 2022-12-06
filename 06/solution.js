121c7bd58bb8996f85c5de4cc4baab73fcb858f5
Jan Pfenning <jan.pfenning@gmail.com>
2022-12-06 19:27:36 +0100

    06

--- 06/solution.js
+++ 06/solution.js
@@ -2,5 +2,19 @@ import * as fs from 'fs';
 const fileName = './input.txt';
 let fileContent = fs.readFileSync(fileName, 'utf8');
 
-const includingPairs = fileContent.split(/\r?\n/)
-includingPairs.forEach(line => console.log(line))
+const input = fileContent.split("")
+const duplicateInSlidingWindow = (slidingWindow) => {
+    for (let i = 0; i < slidingWindow.length; i++) {
+        if(slidingWindow.filter(element => element === slidingWindow[i]).length>1) return true
+    }
+    return false;
+}
+
+input.forEach((_, index) => {
+    if(!duplicateInSlidingWindow(
+        Array.from(Array(14).keys()).map(element => input[index-element]))
+    )
+        console.log(index+1)
+})
+//1779
+//2635
